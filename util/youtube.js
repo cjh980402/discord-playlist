0d58ff0d163c54929702eb4786333b80bea279ad
cjh980402 <9804cjh@naver.com>
2024-07-01 09:23:11 +0900

    불필요 모듈 제거

--- util/youtube.js
+++ util/youtube.js
@@ -2,8 +2,6 @@ import { google } from 'googleapis';
 import { PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '../config.js';
 import { sendAdmin } from '../admin/bot_message.js';
 import http from 'http';
-import url from 'url';
-import open from 'open';
 import destroyer from 'server-destroy';
 
 const youtube = google.youtube('v3');
@@ -15,12 +13,12 @@ const redirect_uri = `http://localhost:${PORT}`;
 export const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, redirect_uri);
 
 /**
- * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
+ * This is one of the many ways you can configure googleapis to use authentication credentials. In this method, we're setting a global reference for all APIs.
  */
 google.options({ auth: oauth2Client });
 
 /**
- * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
+ * Open an http server to accept the oauth callback.
  */
 export async function authenticate(scopes, client) {
     return new Promise((resolve, reject) => {
@@ -34,20 +32,17 @@ export async function authenticate(scopes, client) {
         const server = http
             .createServer(async (req, res) => {
                 try {
-                    const qs = new url.URL(req.url, redirect_uri).searchParams;
+                    const qs = new URL(req.url, redirect_uri).searchParams;
                     res.end('Authentication successful! Please return to the console.');
                     server.destroy();
                     const { tokens } = await oauth2Client.getToken(qs.get('code'));
                     oauth2Client.setCredentials(tokens);
                     resolve(oauth2Client);
-                } catch (e) {
-                    reject(e);
+                } catch (err) {
+                    reject(err);
                 }
             })
-            .listen(PORT, () => {
-                // open the browser to the authorize url to start the workflow
-                open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
-            });
+            .listen(PORT, () => console.log(`${PORT}번 port에 http server를 띄웠습니다.`));
         destroyer(server);
     });
 }
